1c6495edbb3c68efc065d9f11fd664884e8c9b86
Aman <amanpatel9824@gmail.com>
2024-01-20 12:37:14 -0500

    Updating Job Description

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,9 +10,10 @@ function Work() {
         <p className="job__title">Software Developer</p>
         <p className="company__name">Amdocs | Jan 2022 - Present</p>
         <ul className="job__points">
-          <li> Spearheaded the development of critical modules for the CRM application using Java and Spring Boot, resulting in a 20% improvement in system efficiency. </li>
-          <li> Implemented automated testing procedures with JUnit and Mockito, achieving a 30% reduction in debugging time and ensuring 100% code coverage. </li>
-          <li> Collaborated with cross-functional teams in peer code reviews, utilizing Git and Bitbucket, significantly improving code quality and adherence to best practices. </li>
+          <li> Part of the Development and delivery Unit responsible for the CRM application for the telecom base customers. Involving E2E implementation activity starting from development to delivery. Maintained clean code practice and with 100% aim on automated test coverage. </li>
+          <li> Spearheaded the design, development, and maintenance of software code, ensuring compliance with industry standards and optimizing processes through the implementation of DevOps practices. </li>
+          <li> Investigated and swiftly resolved issues, conducting thorough code reviews, and debugging, leading to a 30% reduction in system downtime and a 20% increase in overall software reliability. </li>
+          <li> Facilitated seamless collaboration within cross-functional teams, leading to efficient decision-making and prompt issue resolution, fostering a cohesive work environment. </li>
         </ul>
       </div>
 
@@ -30,8 +31,8 @@ function Work() {
         <p className="job__title">Software Developer Intern</p>
         <p className="company__name">The Graduate Network | Jan 2021 - June 2021</p>
         <ul className="job__points">
-          <li> Contributed significantly to the development of scalable, fault-tolerant distributed storage, index, and query systems using Java and Python, resulting in a 40% improvement in system performance. </li>
-          <li> Led the adoption of best practices in software engineering, including coding standards and rigorous testing with JUnit, Mockito, and other tools, resulting in a 20% reduction in post-release defects. </li>
+          <li> Contributed significantly to the development of scalable, fault-tolerant distributed storage, index, and query systems using Java and Python on low cost, and easy to manage/use. </li>
+          <li> Got hands-on experience in professional software engineering practices & best practices for the full software development life cycle, including coding standards, code reviews, source control management, build processes, testing, and operations. </li>
           <li> Received recognition for proactive problem-solving and collaboration, directly impacting the successful completion of project milestones ahead of schedule. </li>
         </ul>
       </div>
