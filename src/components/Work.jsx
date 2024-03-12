65ced5255486dd48eb088ed98490f6856b3b8b1c
Aman <amanpatel9824@gmail.com>
2024-03-12 15:19:11 -0400

    Updating Job Description

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,10 +10,12 @@ function Work() {
         <p className="job__title">Software Developer</p>
         <p className="company__name">Amdocs | Jan 2022 - Present</p>
         <ul className="job__points">
-          <li> Part of the Development and delivery Unit responsible for the CRM application for the telecom base customers. Involving E2E implementation activity starting from development to delivery. Maintained clean code practice and with 100% aim on automated test coverage. </li>
-          <li> Spearheaded the design, development, and maintenance of software code, ensuring compliance with industry standards and optimizing processes through the implementation of DevOps practices. </li>
-          <li> Investigated and swiftly resolved issues, conducting thorough code reviews, and debugging, leading to a 30% reduction in system downtime and a 20% increase in overall software reliability. </li>
-          <li> Facilitated seamless collaboration within cross-functional teams, leading to efficient decision-making and prompt issue resolution, fostering a cohesive work environment. </li>
+          <li> Developed and maintained software code with Java and Spring Boot according to functional, non-functional, and technical design specification. </li>
+          <li> Writing clean, maintainable, and testable code with clean code practices with 100% aim on automated test coverages using Mockito and TestNG. </li>
+          <li> Collaborated with cross-function teams to design, develop and implement scalable solutions. </li>
+          <li> Troubleshooted and resolved production and non-production issues by reviewing/debugging code using IntelliJ and Kibana and provided fixes and workarounds in timely manner to ensure deadlines were met. </li>
+          <li> Tested and deployed microservices in OpenShift and Amazon EKS/EC2 environments using Spring Boot, Docker, Helm, and Kubernetes technologies. </li>
+          <li> Performed performance tuning of application on PET environment using AppDynamics and JMeter. </li>
         </ul>
       </div>
 
@@ -21,9 +23,8 @@ function Work() {
         <p className="job__title">Junior Quality Automation Engineer</p>
         <p className="company__name">Equifax | July 2021 - Jan 2022</p>
         <ul className="job__points">
-          <li> Executed comprehensive functional and end-to-end testing procedures using automated scripts, ensuring the accuracy of system responses and outputs in alignment with business requirements. </li>
-          <li> Implemented a streamlined documentation process using tools like Confluence, reducing the time spent on project status updates by 15% and enhancing team communication. </li>
-          <li> Played a key role in identifying and resolving critical system issues, utilizing debugging tools and collaborating with development teams, contributing to a 25% improvement in overall application reliability. </li>
+          <li> Maintained thorough and accurate documentation of all work in Confluence and Jira including status updates to summarize projects highlights validating those processes operate properly and conform to standards. </li>
+          <li> Performed testing of applications based on pre-written scripts created with Bash and Postman, focusing on functional and end-to-end testing, validating that system response and output accurately reflect business requirements as outlined in requirements document. </li>
         </ul>
       </div>
 
@@ -31,9 +32,9 @@ function Work() {
         <p className="job__title">Software Developer Intern</p>
         <p className="company__name">The Graduate Network | Jan 2021 - June 2021</p>
         <ul className="job__points">
-          <li> Contributed significantly to the development of scalable, fault-tolerant distributed storage, index, and query systems using Java and Python on low cost, and easy to manage/use. </li>
+          <li> Worked in an agile environment to deliver high-quality software. </li>
+          <li> Built distributed storage, index, and query systems that are scalable, fault-tolerant, low cost, and easy to manage/use. </li>
           <li> Got hands-on experience in professional software engineering practices & best practices for the full software development life cycle, including coding standards, code reviews, source control management, build processes, testing, and operations. </li>
-          <li> Received recognition for proactive problem-solving and collaboration, directly impacting the successful completion of project milestones ahead of schedule. </li>
         </ul>
       </div>
     </div>
